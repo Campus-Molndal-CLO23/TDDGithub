3871c7c362795758e503f284267c9038b54ddde1
Marcus Medina <medina.jobb@outlook.com>
2023-11-24 10:03:24 +0100

    Add project files.

---
+++ Veckans/readme.md
@@ -0,0 +1,18 @@
+# veckans utmaning
+
+```
+4 klasser
+Meter
+    constructor (value)
+	value {get;set;}
+	static double toDecimeter
+		   toCentimeter
+		   toMillimiter
+	static Meter fromDecimeter(Decimeter value)
+			     fromCentimeter(
+Decimeter
+Centimeter
+Millimeter
+```
+
+# Hur många tester behövs?
---
+++ VeckansTests/CalculatorTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Veckans;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veckans.Tests;
+
+[TestClass()]
+public class CalculatorTests
+{
+    [TestMethod()]
+    public void AddTest()
+    {
+        Assert.Fail();
+    }
+}
